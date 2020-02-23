b6bd58ca90075917736161bf32b017e116ff4e43
parvana07 <peroinitworld@gmail.com>
2020-02-22 20:39:35 -0800

    added functionality to register field and some styling for register field

--- client/App.jsx
+++ client/App.jsx
@@ -10,9 +10,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Login />
+        {/* <Login /> */}
         {/* <Checkbox /> */}
-        {/* <Register /> */}
+        <Register />
       </div>
     );
   }
--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -3,8 +3,39 @@ import React, { Component } from "react";
 class Register extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      school: "",
+      favs: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setInterest = this.setInterest.bind(this);
   }
 
+  handleSubmit() {
+    alert(
+      ` ${this.state.username} ${this.state.firstName} ${this.state.lastName} ${this.state.email} ${this.state.password} ${this.state.school} `
+    );
+    alert(this.state.favs);
+  }
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+  componentDidMount() {
+    this.setState({ favs: this.state.favs });
+  }
+  setInterest() {
+    this.setState(prevState => {
+      return {
+        favs: !prevState.favs
+      };
+    });
+  }
   render() {
     let checkboxes = [];
     const interest = [
@@ -23,7 +54,13 @@ class Register extends Component {
       checkboxes.push(
         <div className="interestCol">
           <span className="checkbox">
-            <input type="checkbox" value={interest[i]} id={interest[i]} />
+            <input
+              type="checkbox"
+              value={interest[i]}
+              name={interest[i]}
+              onClick={this.setInterest}
+              id={i}
+            />
             <label>{interest[i]}</label>
           </span>
         </div>
@@ -33,20 +70,56 @@ class Register extends Component {
 
     return (
       <div className="login-register-input">
-        <form action="">
+        <form onSubmit={this.handleSubmit} action="">
           <h1>Create Account</h1>
-          <input className="text" type="text" placeholder="Username" />
-          <input className="text" type="text" placeholder="First Name" />
-          <input className="text" type="text" placeholder="Last Name" />
-          <input className="text" type="email" placeholder="Email" />
-          <input className="text" type="password" placeholder="Password" />
-          <input className="text" type="text" placeholder="School" />
+          <input
+            className="textReg"
+            name="username"
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            className="textReg"
+            name="firstName"
+            onChange={this.handleChange}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            className="textReg"
+            name="lastName"
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Last Name"
+          />
+          <input
+            className="textReg"
+            name="email"
+            onChange={this.handleChange}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            className="textReg"
+            name="password"
+            onChange={this.handleChange}
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            className="textReg"
+            name="school"
+            onChange={this.handleChange}
+            type="text"
+            placeholder="School"
+          />
           {checkboxes}
           <br />
           <p>
             I agree all statements in <a href="">Term of service.</a>
           </p>
-          <input className="button" type="submit" />
+          <input className="regButton" type="submit" />
         </form>
       </div>
     );
